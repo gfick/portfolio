23d572a282ada01ba2e249303ba181426cb5959b
Gregory Fick <gregory.b.fick@gmail.com>
2015-02-23 16:48:51 -0500

    remove some comments and change res.send to res.render for 500 errors

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var path = require('path');
 
 var app = express();
 
-app.set('views', path.join(__dirname, 'views')); // This is to serve static files like html in the views folder
-app.set('view engine', 'ejs'); // your engine, you can use html, jade, ejs, vash, etc
-app.set('port', process.env.PORT || 3000); // set up the port to be production or 80.
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 3000);
 app.set('env', process.env.NODE_ENV || 'development'); 
 
-app.use(express.static(path.join(__dirname, 'assets'))); // This is to serve static files like .css and .js, images, fonts in the assets folder
+app.use(express.static(path.join(__dirname, 'assets')));
 
 app.get('/', function(req, res) {
   res.render('index');
@@ -22,7 +22,7 @@ app.use(function(req, res) {
   
 // Handle 500
 app.use(function(error, req, res, next) {
-   res.send('error500', 500);
+   res.render('error500', 500);
 });
 
 app.listen(app.get('port'), function(req, res) {
