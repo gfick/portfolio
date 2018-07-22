1935ab776b3b8069271249f1753566c392847d6c
Greg <gregory.b.fick@gmail.com>
2018-07-21 21:10:34 -0400

    nav

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,21 +1,16 @@
 $(document).ready(function(){
 
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-    console.log('here');
-  });
-
   $('a[href^="#"]').on('click',function (e) {
-      e.preventDefault();
+    e.preventDefault();
 
-      var target = this.hash;
-      var $target = $(target);
+    var target = this.hash;
+    var $target = $(target);
 
-      $('html, body').stop().animate({
-          'scrollTop': $target.offset().top
-      }, 900, 'swing', function () {
-          document.getElementById("about-me").scrollIntoView(true);
-      });
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 900, 'swing', function () {
+        document.getElementById("about-me").scrollIntoView(true);
+    });
   });
 
 });
